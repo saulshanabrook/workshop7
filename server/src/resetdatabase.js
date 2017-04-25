d0ba0ebb40415e60298f3f156fb87df328154d79
Saul Shanabrook <s.shanabrook@gmail.com>
2017-04-25 16:28:53 -0400

    databased

--- server/src/resetdatabase.js
+++ server/src/resetdatabase.js
@@ -108,6 +108,12 @@ var initialData = {
 };
 
 /**
+ * Adds any desired indexes to the database.
+ */
+function addIndexes(db, cb) {
+  db.collection('feedItems').createIndex({ "contents.contents": "text" }, null, cb);
+}
+/**
  * Resets a collection.
  */
 function resetCollection(db, name, cb) {
@@ -143,8 +149,8 @@ function resetDatabase(db, cb) {
       // Use myself as a callback.
       resetCollection(db, collection, processNextCollection);
     } else {
-      cb();
-    }
+  addIndexes(db, cb);
+}
   }
 
   // Start processing the first collection!
